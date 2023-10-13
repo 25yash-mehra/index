b8cefe6e6917fa63467934e103e7a166c54eae4b
red wolf <myash7535@gmail.com>
2023-10-13 11:25:59 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,505 @@
+// console.log('hello world');
+// console.log('lakhan singh');
+// console.log('here are u');
+
+// var x = 'warning';
+// var y = 25;
+// x = "this is so hard"
+// console.log(x);
+// console.log(y);
+// var first,second
+// first = "yash"
+// second = "25"
+// console.log(first,second);
+// let x = 'yash';
+// let x = 'mehra';
+// console.log(x);
+// const fname = "red wolf";
+// fname = "yash";
+// console.log(fname);
+// var x = ["yash","mehra","wolf"];
+// console.log(typeof x);
+// var sum = 10 + "14";
+// console.log(10 - "20");
+// console.log(4 - "2");
+// console.log("red"  + "wolf");
+// console.log( " " + 0);
+// console.log( false - true);
+// var a = 12;
+// var b = 2;
+// a++;
+// console.log(a + b);
+// var a = 24;
+// var b = 10;
+// a %= b;
+// console.log(a);
+
+// console.log("<br>");
+// document.getElementById("yash") .innerHTML
+// console.warn(a)
+// console.error(a);
+
+
+// var a = 10;
+// var b = 30;
+// // console.log( 30 != 30);
+
+// console.log( a<b && b>a );
+// console.log( 20 > 10 || 5 < 4);
+// console.log(!( (10>20) || (20<10) ));
+// console.log(3 ** 3);
+// console.log( 26 + "yash");
+//  let x = 40;
+// let  y = 30;
+// if( x > y){
+//     console.log( "yes");
+// }
+// let p = 20;
+// let q = 30;
+// if( p < q && q > p){
+//     console.log( "yes this is ture");
+// }
+// let j = 30;
+// let o = 40;
+// if( j < o || o < j){
+//     console.log(" this is write answer");
+// }
+//  let m = 40;
+//  let n = 50;
+// //  if ( m < n){
+//     console.log( !(m < n));
+// //  }
+
+// let f = 30; 
+// if( f >40){
+//     console.log( "this is false");
+// } else {
+//     console.log( " this is true");
+// }
+
+// let name1 = 'yash';
+// let usergender = "male";
+// if( usergender == "male"){
+//     console.log( " hello mr." + name1);
+// } else{
+//     console.log(" hello miss" + name1);
+// }
+
+
+// let name25 = "yash";
+// let ling = 'male';
+// if( ling == 'male'){
+//     console.log("hello mr" + name25);
+// } else {
+//     console.log(( "hello miss" + name25));
+// }
+
+// // let marks = prompt (" your percentage");
+
+// if( marks >= 80 && marks <= 100){
+//     console.log(" your are in merit");
+// }
+// else if(marks >= 60 && marks < 79 ){
+//     console.log( " you are in  first division");
+// }
+// else if(marks >= 45 && marks < 59){
+//     console.log( " you are in  second division");
+// }
+// else if(marks >= 33 && marks < 44){
+//     console.log( " you are in  third division");
+// }
+// else if( marks < 33){
+//     console.log( "you are fail");
+// }
+// else{
+//     console.log(" please enter valid roll number");
+// }
+// prompt (" what is your name")
+
+
+
+
+//  function Yash( name, surname ){
+
+//     document.write( name +" "+ surname + " </br>");
+
+//  }
+//  Yash('yash', 'sharma'); 
+// Yash('shanti','prasad');
+
+// function yash() {
+//    document.write("hello wolf is here" );
+// }
+// function wolf() {
+//    document.write("hello yash is here" );
+// }
+// yash(); 
+// document.write("<br>")
+// yash(); 
+// document.write("<br>")
+// yash();
+// document.write("<br>")
+// wolf();
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// function red(fname , lname) {
+//    document.write(fname + lname + "<br>");
+// // }
+// // red(" yahs"," mehra");
+// // red(" yahs"," singh");
+
+// // function red( fname , lname ) {
+// //    document.write( fname + lname + "<br>");
+// // }
+// // red( "happy brthday", "lakhan", "banna ji");
+
+// // function birthday (wishes , fname , lname) {
+// //    document.write(wishes + fname + lname);
+// // }
+// // birthday("happy birthday" , " lakhan", " banna ji");
+
+// // Runner.instance_.gameOver=()=>{}
+// // Runner.instance_.setSpeed(1000)
+
+// // function red(){
+// //    document.write("this is");
+// // }
+// // red();
+
+
+// // function wolf( fname , lname){
+// //    document.write(fname + lname + "<br>");
+// // }
+// // wolf( "yash" , " mehra");
+// // wolf("sidhu" , " moose wala");
+
+// // function king(){
+// //    let k = "im a Kira";
+// //    document.write(k + "<br>");
+// // }
+// // king();
+// // document.write(k);
+
+// // function kira(){
+// //    alert("welcome to our website");
+// // }
+
+// // let x = 1;
+// // while (x <= 10){
+// //    document.write(x +  ") this is  loop <br>" );
+// //    x++;
+// // }
+
+// // let a = 1;
+// // do{
+// //    document.write("this is loop");
+// //    a++;
+// // }
+// // while ( a <= 10); 
+
+// console.log("yash");
+
+// // prompt("what do you want")
+// // alert("hwllow")
+// console.time("yash");
+// let a = "yash";
+// console.log(typeof a);
+// console.timeEnd("yash");
+// let b = 20;
+// console.log(typeof b);
+// let x = true;
+// console.log(typeof x);
+// let y = { first: "yash" };
+// console.log(typeof y);
+// let g = 2;
+// let h = 23;
+// g += h;
+// console.log(g)
+
+// let per = 30;
+// if (per >= 90 && per <= 100) {
+//     console.log("your are in merit list");
+// }
+// else if (per >= 80 && per < 90) {
+//     console.log("your are amazing");
+// }
+// else if (per >= 70 && per < 80) {
+//     console.log("your are excelent");
+// }
+// else if (per >= 60 && per < 70) {
+//     console.log("you are good");
+// }
+// else if (per >= 50 && per < 60) {
+//     console.log("your are average");
+// }
+// else if (per >= 40 && per < 50) {
+//     console.log(" your are bad");
+// }
+// else if (per >= 30 && per < 40) {
+//     console.log(" you have to improve more");
+// }
+// else {
+//     (per < 30)
+//     console.log("you are fail");
+// }
+
+// // confirm("are you sure")
+// function yash() {
+//     console.log("wolf <br>");
+// }
+// yash();
+// yash();
+// function red(fname, lname) {
+//     console.log(fname + lname);
+// }
+// red("rohit", "verma")
+// red("karan", "johar")
+
+
+// let sensi = "this is peremiter loop";
+// function kira() {
+//     console.log(sensi);
+// }
+// kira();
+// kira();
+// console.log(sensi);
+
+// function kill(){
+// alert
+// }
+
+// for(let m = 1;  m <= 10; m++){
+//     document.write("for loop <br> ")
+// }
+// for(let n=1; n <= 10; n++){
+//     document.write(n*2 + "</br>" );
+// }
+
+
+
+// program to generate a multiplication table
+
+// take input from the user
+
+
+// let table = prompt("enter the integer")
+// for(let sum = 1; sum <= 10; sum++){
+//     document.write(table + "*" + sum + "=" + table*sum + "<br>");
+//     // 2*1=2
+// }
+
+// for( let wolf = 1; wolf <= 20; wolf++ ){
+//     if (wolf % 2 == 0) {
+//         document.write(wolf + "<br>")
+//     }
+// }
+
+// for(let x = 1; x<=10; x++ ){
+//     if ( x == 3) {
+//         document.write("hey : " + x + "<br>");
+//        break;
+//     }
+//     document.write("number : " + x + "<br>");
+// }
+
+
+// for(let p=1; p<=100; p=p+10){
+//     for(let q=p; q<p+10; q++){
+//  document.write(q);
+//     }
+//     document.write("<br>")
+// }
+
+// var yash = new Array();
+// yash [0] = 20;
+// yash [1] = 50;
+// yash [2] = 40;
+
+
+// for(g=0; g<3; g++){
+//     yash[g] = prompt("enter the value");
+// }
+
+// for(var v=0; v<=3; v++){
+// document.write(yash[v] + "<br>")
+// }
+
+// var red = [
+//     ["yash", "jaipur"],
+//     ["rahul", "kolkata"],
+//     ["prachi", "behror"],
+// ]
+// for (let z = 0; z < 3; z++) {
+//     console.log('fired')
+//     for (let y = 0; y < 2; y++) {
+//         console.log('fired again')
+//         document.write(`${z} and ${y} <br>`);
+//     }
+
+// }
+// red.map((item) => {
+//     console.log(item)
+// })
+// let r = [12,13,14,15,17,19,20,23];
+// let s = r.filter(age);
+// document.write(s);
+// function age(s) {
+//     return s >= 18;
+// }
+
+
+// let s = ["yash","lakhan","urus","supra","tata"];
+// s.forEach(loop);
+// function loop(value, index) {
+//     document.write( index + " : " + value + "<br>" )
+// }
+
+// let s = {
+//     fname : "yash", // fname is properties 
+//     lname : "mehra",
+//     city : "jaipur",
+//     study : ["bba","html","css","javasript"],
+//     salary : function() { // method
+//         return 15000 + 200;
+//     },
+
+//       fullname : function () {
+//     return this.fname + " " + this.lname;
+//       },
+//      country : {
+//         seher : "amer",
+//         jagha : "udaipur",
+//      }  
+// }
+// document.write(s.country.jagha + "<br>")
+// document.write(s.salary());
+// // salary();
+// document.write(s.fullname());
+
+// let p = new Object();
+// p.fname = "yash";
+// p.lname = "mehra";
+// p.age = 19;
+// p.country = "jaipur";
+
+// document.write(p.age + "<br>");
+// document.write(p.country)
+
+// let paras = [
+//     {name:"yash",age:14,country:"jaipur"},
+//     {name:"harsh",age:10,country:"punjab"},
+//     {name:"priyanka",age:19,country:"chandigrah"},
+// ]
+// console.log(paras);
+
+
+// document.write("yash" + "<br>");
+
+// let r = 23;
+//  r += 12;
+// document.write(r + "<br>");
+
+// let yogesh = 102;
+// let harsh = 10;
+// document.write(!(yogesh > harsh || harsh < yogesh));
+// function yash() {
+//     document.write("good one");
+// }
+
+
+// let r = 12;
+// let s = 10;
+// document.write(r>s || r < s);
+// if (r>s) {
+//     document.write("yash")
+// }
+
+
+// let k = 20;
+// let l = 30;
+// // document.write(!(k > l || l < k));
+
+// if (k 
+//     < l) {
+//     document.write("yash");
+// } else {
+//     document.write("wolf");
+// }
+
+//  let a = 10;
+//  let b = 9;
+// //  document.write(a>b && b<a);
+//  if (a<b) {
+//     document.write("yash");
+//  }else{
+//     document.write("wolf");
+//  }
+
+// let per = prompt("enter the number here")
+// if (per >= 90 && per <= 100) {
+//     document.write("your are in merit");
+// }
+// else if ( per >= 80 && per < 90 ){
+//     document.write("you are amazing");
+// }
+// else if (per >= 70 && per < 80){
+//     document.write("you done better");
+// }
+// else if ( per >= 60 && per < 70){
+//     document.write("you done average");
+// }
+// else if (per >= 50 && per < 60){
+//     document.write("you done good");
+// }
+
+// else if (per >= 40 && per < 50){
+//     document.write("you done normal");
+// }
+
+// else if (per >= 30 && per < 40){
+//     document.write("you done shit");
+// }
+// else{
+//     (per<30);
+//     document.write("bhai fail he tu");
+// }
+
+// for(let a = 1; a<=20; a++){
+// if (a % 2 == 0) {
+//     document.write(a + "<br>")
+// }
+// }
+// for( let wolf = 1; wolf <= 20; wolf++ ){
+//     if (wolf % 2 == 0) {
+//         document.write(wolf + "<br>")
+//     }
+// }
+// 2*1=2
+// for(let a = 1; a<=10; a++){
+//     table = 2;
+//     document.write( table +  "*" + a + "=" + a*table  + "<br>")
+// }
+
+// function yash() {
+//     document.write("red wolf.")
+// }
+// yash();
+
+// function red(fname , lname) {
+//     document.write(fname+lname);
+// }
+// red("yash","mehra"+"<br>");
+// red("red","wolf"+"<br>")
+// red("harsh "," saini")
+
+var student = [
+    { name: 'Ram', age: 15 },
+    { name: 'Karan', age: 13 },
+    { name: 'Rahul', age: 14 },
+];
+
+console.log(student.length);
+for (var a = 0; a < student.length; a++) {
+    document.write(student[a].name + " " + student[a].age + "<br>");
+}
